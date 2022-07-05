9e60c48926da29bf09fd474baaa8365b1571c235
Daniela  Ramirez <danielaramirez@Danielas-Air-2.attlocal.net>
2022-07-05 11:58:55 -0400

    Easy and very easy challenge done

---
+++ Easy/easy.js
@@ -0,0 +1,6 @@
+var person1 = "Justin"; 
+var person2 = "Dylan";
+
+if(person1.length > person2.length){
+    console.log(`${person1}'s name is longer than ${person2}'s name by ${person1.length - person2.length} character's`)
+}
